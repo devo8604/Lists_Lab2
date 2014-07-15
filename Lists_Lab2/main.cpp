cee9a97020ad84354234a416303692414b9caa90
devo8604 <devo8604@gmail.com>
2014-07-14 20:25:20 -0600

    Initial Commit

---
+++ Lists_Lab2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Lists_Lab2
+//
+//  Created by Devon Smith on 7/14/14.
+//  Copyright (c) 2014 Devon Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
